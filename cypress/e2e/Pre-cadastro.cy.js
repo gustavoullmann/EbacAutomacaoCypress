954e070be9430d08584041f90f4b0056f915818b
Gustavo Ullmann <gustavo.ullmann@cwi.com.br>
2023-09-17 20:05:28 -0300

    Criação da suite de testes de Pre-cadastro

--- cypress/e2e/Pre-cadastro.cy.js
+++ cypress/e2e/Pre-cadastro.cy.js
@@ -1,1 +1,57 @@
 /// <reference types = "cypress"/>
+
+const { faker } = require('@faker-js/faker');
+const baseUrl = Cypress.env('baseUrl') + 'minha-conta/'
+var firstName = faker.person.firstName()
+var lastName = faker.person.lastName()
+var email = faker.internet.exampleEmail({ firstName: `${firstName}`, lastName: `${lastName}` })
+var password = faker.internet.password({ length: 12 })
+
+describe('Testa a página de Pré-cadastro da Ebac', () => {
+
+    beforeEach(() => {
+        cy.visit(baseUrl)
+    });
+
+    it('Completa o pré-cadastro com sucesso', () => {
+        cy.get('#reg_email')
+            .type(email)
+        cy.get('#reg_password')
+            .type(`${password}{enter}`)
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a')
+            .click()
+        cy.get('#account_first_name')
+            .type(firstName)
+        cy.get('#account_last_name')
+            .type(lastName)
+        cy.get('.woocommerce-Button')
+            .click()
+        cy.get('.woocommerce-message')
+            .should('contain', 'modificados com sucesso')
+    });
+
+    it('Não aceita cadastro de usuário já registrado', () => {
+        cy.get('#reg_email')
+            .type(email)
+        cy.get('#reg_password')
+            .type(`${password}{enter}`)
+        cy.get('.woocommerce-error > li')
+            .should('contain', 'conta já está registrada')
+    });
+
+    it('Não aceita cadastro com email vazio', () => {
+        cy.get('#reg_password')
+            .type(`${password}{enter}`)
+        cy.get('.woocommerce-error > li')
+            .should('contain', 'Informe um endereço de e-mail válido')
+    });
+
+    it('Não aceita cadastro com senha vazia', () => {
+        var newEmail = faker.internet
+            .email()
+        cy.get('#reg_email')
+            .type(`${newEmail}{enter}`)
+        cy.get('.woocommerce-error > li')
+            .should('contain', 'Digite a senha da conta')
+    })
+});
