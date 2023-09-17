cde866bc030d177b0f1ae55b069ec50871c2872e
Gustavo Ullmann <gustavo.ullmann@cwi.com.br>
2023-09-17 20:04:14 -0300

    Ajuste de formatação da suite login

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -9,37 +9,55 @@ describe('Testa a página de Login da Ebac', () => {
     beforeEach(() => {
         cy.visit(baseUrl)
     });
-   
+
     it('Faz login com sucesso', () => {
-        cy.get('#username').type(validUsername)
-        cy.get('#password').type(`${validPassword}{enter}`)
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá')
+        cy.get('#username')
+            .type(validUsername)
+        cy.get('#password')
+            .type(`${validPassword}{enter}`)
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)')
+            .should('contain', 'Olá')
     });
 
     it('Exibe mensagem de erro ao inserir usuário inválido', () => {
-        cy.get('#username').type('usuario@invalido.com')
-        cy.get('#password').type(`qualquerSenha{enter}`)
-        cy.get('.woocommerce-error > li').should('contain', 'e-mail desconhecido')
+        cy.get('#username')
+            .type('usuario@invalido.com')
+        cy.get('#password')
+            .type(`qualquerSenha{enter}`)
+        cy.get('.woocommerce-error > li')
+            .should('contain', 'e-mail desconhecido')
     });
 
     it('Exibe mensagem de erro ao inserir senha inválida', () => {
-        cy.get('#username').type(validUsername)
-        cy.get('#password').type(`qualquerSenha{enter}`)
-        cy.get('.woocommerce-error > li').should('contain', 'Perdeu a senha?') 
+        cy.get('#username')
+            .type(validUsername)
+        cy.get('#password')
+            .type(`qualquerSenha{enter}`)
+        cy.get('.woocommerce-error > li')
+            .should('contain', 'Perdeu a senha?')
     });
 
     it('Faz logout com sucesso', () => {
-        cy.get('#username').type(validUsername)
-        cy.get('#password').type(`${validPassword}{enter}`)
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá')
-        cy.get('.topbar-inner > :nth-child(1) > .list-inline > :nth-child(2) > a').click()
-        cy.url().should('eq', 'http://lojaebac.ebaconline.art.br/')
+        cy.get('#username')
+            .type(validUsername)
+        cy.get('#password')
+            .type(`${validPassword}{enter}`)
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)')
+            .should('contain', 'Olá')
+        cy.get('.topbar-inner > :nth-child(1) > .list-inline > :nth-child(2) > a')
+            .click()
+        cy.url()
+            .should('eq', 'http://lojaebac.ebaconline.art.br/')
     });
 
     it('Envia com sucesso e-mail de recuperação de senha', () => {
-        cy.get('.lost_password > a').click()
-        cy.get('#user_login').type(validUsername)
-        cy.get('.woocommerce-Button').click()
-        cy.get('.woocommerce-message').should('contain', 'redefinição de senha')
+        cy.get('.lost_password > a')
+            .click()
+        cy.get('#user_login')
+            .type(validUsername)
+        cy.get('.woocommerce-Button')
+            .click()
+        cy.get('.woocommerce-message')
+            .should('contain', 'redefinição de senha')
     });
 });
